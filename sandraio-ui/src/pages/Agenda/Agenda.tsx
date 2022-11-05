b58ea1fd4c0acf93a5655b926c12a8dff2df82e0
BarbaraMAlmeida <barbara.nicollealmeida@gmail.com>
2022-11-04 22:10:09 -0300

    Agendamento de aulas

--- sandraio-ui/src/pages/Agenda/Agenda.tsx
+++ sandraio-ui/src/pages/Agenda/Agenda.tsx
@@ -1,7 +1,143 @@
+import BaseContainer from "../../component/BaseContainer/BaseContainer";
+import { ArrowRightOutlined, FileTextOutlined } from "@ant-design/icons";
+import { Button, DatePicker, Form, Input, Modal, Select } from "antd";
+import { Link } from "react-router-dom";
+import { useCallback, useEffect, useState } from "react";
+import { RequiredMark } from "antd/lib/form/Form";
+
 export default function Agenda() {
- return (
-    <div>
-        Agendamento
+  const [form] = Form.useForm();
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+  const [requiredMark, setRequiredMarkType] =
+    useState<RequiredMark>("optional");
+
+  const onRequiredTypeChange = ({
+    requiredMarkValue,
+  }: {
+    requiredMarkValue: RequiredMark;
+  }) => {
+    setRequiredMarkType(requiredMarkValue);
+  };
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <div className="bg-white p-5 m-5 rounded-md flex flex-col w-11/12">
+      <div className="flex justify-between  align-middle w-full mb-3">
+        <div className="flex align-middle">
+          <span className="bg-logoDarkPurple w-9 h-9 text-center leading-7 rounded-md">
+            <FileTextOutlined style={{ color: "#ffffff", fontSize: "12px" }} />
+          </span>
+          <p className="mt-2 ml-2 text-base">Página de Agendamento</p>
+        </div>
+        <Button
+          onClick={() => showModal()}
+          type="default"
+          className="filter-button flex align-middle bg-logoDarkPurple rounded-md border-none"
+          style={{ color: "#ffffff" }}
+        >
+          <span
+            style={{
+              textDecoration: "none",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <span className="mr-2">Agendar</span>
+            <ArrowRightOutlined />
+          </span>
+        </Button>
+      </div>
+
+      <Modal
+        title="Agendamento de aulas"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <Form
+          className={"p-0"}
+          form={form}
+          layout="vertical"
+          initialValues={{ requiredMarkValue: requiredMark }}
+          onValuesChange={onRequiredTypeChange}
+          requiredMark={requiredMark}
+        >
+          <Form.Item label="Professor" required>
+          <Select
+              showSearch
+              size="large"
+              placeholder="Selecione um Professor"
+              optionFilterProp="children"
+              onChange={(e) => console.log(e.target.value)}
+              onSearch={(e) => console.log(e)}
+              filterOption={(input, option) =>
+                (option?.label ?? "")
+                  .toLowerCase()
+                  .includes(input.toLowerCase())
+              }
+              options={[
+                {
+                  value: "1",
+                  label: "Sandra Alves",
+                },
+                {
+                  value: "2",
+                  label: "Luciene Silva",
+                },
+                {
+                  value: "3",
+                  label: "Wladmir",
+                },
+              ]}
+            />
+          </Form.Item>
+          <Form.Item label="Matéria" required>
+          <Select
+              showSearch
+              size="large"
+              placeholder="Selecione uma Matéria"
+              optionFilterProp="children"
+              onChange={(e) => console.log(e.target.value)}
+              onSearch={(e) => console.log(e)}
+              filterOption={(input, option) =>
+                (option?.label ?? "")
+                  .toLowerCase()
+                  .includes(input.toLowerCase())
+              }
+              options={[
+                {
+                  value: "1",
+                  label: "Matemática",
+                },
+                {
+                  value: "2",
+                  label: "Geografia",
+                },
+                {
+                  value: "3",
+                  label: "Química",
+                },
+              ]}
+            />
+          </Form.Item>
+          <Form.Item label="Data" required>
+          <DatePicker className="w-full" size="large"  placeholder="Informe uma Data" />
+          </Form.Item>
+        </Form>
+      </Modal>
     </div>
- )
+  );
 }
