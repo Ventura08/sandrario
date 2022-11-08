5a79f9e53f38974a4f43f0965d0aa1da5466b5cd
GlatzCampos <gabrielglatzc@gmail.com>
2022-11-08 07:41:36 -0300

    html_equipePage

--- sandraio-ui/src/pages/Equipe/Equipe.tsx
+++ sandraio-ui/src/pages/Equipe/Equipe.tsx
@@ -1,7 +1,33 @@
 export default function Equipe() {
     return (
-       <div>
-           Equipe
+       <div>    
+           <div>
+            <div className="">
+                <div>
+                    <div className="bg-black w-32 h-32">
+                    <h1>Nome</h1>
+                    <p>Description</p>
+                </div>
+                <div>
+                    <img src="" alt="" />
+                    <h1>Nome</h1>
+                    <p>Description</p>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <img src="" alt="" />
+                    <h1>Nome</h1>
+                    <p>Description</p>
+                </div>
+                <div>
+                    <img src="" alt="" />
+                    <h1>Nome</h1>
+                    <p>Description</p>
+                </div>
+            </div>
+           </div>
        </div>
+    </div>
     )
-   }
+}
