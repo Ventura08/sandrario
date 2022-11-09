fda84d5f5ed719550130672164f82addc12bf650
Ventura08 <estevaotevu08@gmail.com>
2022-11-09 18:20:42 -0300

    deleting unecessary folders

--- sandrario-api/src/interfaces/DisciplinaInterface.ts
+++
@@ -1,6 +0,0 @@
-export interface Disciplina {
-    id: number;
-    nome: string;
-    hora_aula: number;
-    aulas_totais: number;
-}
--- sandrario-api/src/interfaces/ProfessorInterface.ts
+++
@@ -1,7 +0,0 @@
-import { Disciplina } from "./DisciplinaInterface";
-
-export interface Professor{
-    id: string;
-    nome: string;
-    disciplina: Disciplina;
-}
