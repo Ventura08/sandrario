4151f0d239e278065b5246798ba98591724c2944
Rafa Mendes <rafa.mendesmf@gmail.com>
2022-11-08 22:47:40 -0300

    css ajustado

--- sandraio-ui/src/pages/Equipe/Equipe.tsx
+++ sandraio-ui/src/pages/Equipe/Equipe.tsx
@@ -1,18 +1,23 @@
+import Babi from '../../assets/equipe/babi.jpg'
+import Glatz from '../../assets/equipe/glatz.jpeg'
+import Rafa from '../../assets/equipe/rafa.jpeg'
+import Tevo from '../../assets/equipe/tevo.jpeg'
+
 export default function Equipe() {
     return (
         <div>
             <div className="equipe__box--flex">
 
                 <div className="equipe--box">
-                    <img className="equipe--box-img" src="" alt="Imagem do integrante" />
-                    <h1>Nome 01</h1>
-                    <p>Description</p>
+                    <img className="equipe--box-img" src={Babi} alt="Imagem do integrante" />
+                    <h1>Bárbara Almeida</h1>
                 </div>
 
                 <div className="equipe--box">
-                    <img className="equipe--box-img" src="" alt="Imagem do integrante" />
-                    <h1>Nome 02</h1>
-                    <p>Description</p>
+                    <div className="equipe--box-img">
+                        <img className="img-tevo" src={Tevo} alt="Imagem do integrante" />
+                    </div>
+                    <h1>Estevao Boaventura</h1>
                 </div>
 
             </div>
@@ -20,18 +25,18 @@ export default function Equipe() {
             <div className="equipe__box--flex">
 
                 <div className="equipe--box">
-                    <img className="equipe--box-img" src="" alt="Imagem do integrante" />
-                    <h1>Nome 03</h1>
-                    <p>Description</p>
+                    <img className="equipe--box-img" src={Glatz} alt="Imagem do integrante" />
+                    <h1>Gabriel Glatz</h1>
                 </div>
 
                 <div className="equipe--box">
-                    <img className="equipe--box-img" src="" alt="Imagem do integrante" />
-                    <h1>Nome 04</h1>
-                    <p>Description</p>
+                    <img className="equipe--box-img" src={Rafa} alt="Imagem do integrante" />
+                    <h1>Rafaela Mendes</h1>
                 </div>
 
             </div>
+
+            <h1 className="copy">© Copyright 2022 | Sandrário - Todos os direitos reservados</h1>
         </div>
     )
 }
