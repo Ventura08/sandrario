b57259a7e46df32a1b3d5504e1e5e067386364c3
BarbaraMAlmeida <barbara.nicollealmeida@gmail.com>
2022-11-13 18:42:13 -0300

    consumo endpoints get

---
+++ sandraio-ui/src/interfaces/disciplinaInterface.tsx
@@ -0,0 +1,6 @@
+export interface DisciplinaInterface {
+    id: number
+    nome: string
+    hora_aula: string
+    aulas_totais: number
+}
--- sandraio-ui/src/providers/index.tsx
+++ sandraio-ui/src/providers/index.tsx
@@ -2,5 +2,5 @@ import axios from "axios";
 
 //instancia do axios na porta do backend
 export const Api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "http://localhost:3000/",
 });
